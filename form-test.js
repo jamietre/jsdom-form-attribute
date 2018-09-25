c9469cd2c0fb2f8f026296a13cf5b3d55097abb8
James Treworgy <jamietre@gmail.com>
2018-09-25 19:16:46 -0400

    remove mocha

--- form-test.js
+++ form-test.js
@@ -9,18 +9,25 @@ const html = fs.readFileSync(__dirname+'/test.html', 'utf-8');
 const dom = new JSDOM(html, { virtualConsole });
 
 const document = dom.window.document;
+const p1 = new Promise(resolve => {
+  document.getElementById('bar').addEventListener('submit', () => {
+    resolve();
+  });
+}).then(() => console.log('regular form submit event received'))
 
-describe('JSDOM works with "form" attribute', () => {
-  it('submit event works with a normal form', (done) => {
-    document.getElementById('bar').addEventListener('submit', () => {
-      done();
-    });
-    document.getElementById('bar-button').click();
+const p2 =  new Promise(resolve => {
+  document.getElementById('foo').addEventListener('submit', () => {
+    resolve();
   });
-  it('submit works with "form" attribute', (done) => {
-    document.getElementById('foo').addEventListener('submit', () => {
-      done();
-    });
-    document.getElementById('foo-button').click();
-  });
-});
+}).then(() => console.log('form attribute submit event received'))
+
+document.getElementById('bar-button').click();
+document.getElementById('foo-button').click();
+
+Promise.all([p1, p2]).then(() => {
+  console.log('both promises resolved');
+})
+
+setTimeout(() => {
+  console.log('done waiting');
+}, 1000)
